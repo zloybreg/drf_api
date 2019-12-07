0ccd04103ebdefc442ddd65138f15bbf1f432c3c
dmitry <zloybreg@gmail.com>
2019-12-08 02:23:13 +0700

    Add User model.
    Add User model. Add some unit tests. Add dockerignore file. Correct gitignore file

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email(self):
+        """Test creating a new user"""
+        email = 'test@mail.ru'
+        password = 'pass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """Test email for new user is normalize"""
+        email = 'test@MAIL.RU'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating with no user email then raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@mail.Ru',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
